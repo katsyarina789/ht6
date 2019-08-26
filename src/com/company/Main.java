6d82b08a42e2a2514a02c931dd8419c801807671
katsyarina789 <katsyarina.fedorova97@gmail.com>
2019-08-26 23:13:35 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+
+         for (a = 3, b = 7; a <= 100; a += 3, b += 7)
+         if (b <=100)
+
+         System.out.println((a + b) / a * b);
+//Не понимаю, почему у меня делит до 300, а не до 100
+
+            }
+            }
+
+
