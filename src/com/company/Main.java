c26aac1c75c24567741c4a02a6d83267ae0fa847
katsyarina789 <53409343+katsyarina789@users.noreply.github.com>
2019-08-27 19:37:40 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 3;
-        int b = 7;
+        
+        System.out.println("1)");
+        for (int i = 1; i <= 100; i++)
+            if (i % 3 == 0 & i % 7 == 0)
+                System.out.println(i + "");
 
-         for (a = 3, b = 7; a <= 100; a += 3, b += 7)
-         if (b <=100)
-
-         System.out.println((a + b) / a * b);
-//Не понимаю, почему у меня делит до 300, а не до 100
+            for (int i = 1; i<=100; i++){
+                if (i % 3 == 0 && i % 5 == 0)
+                    continue;
+                if (i % 3 == 0|| i % 5 == 0)
+                    System.out.println(i + "");
+            }
             }
             }
 
